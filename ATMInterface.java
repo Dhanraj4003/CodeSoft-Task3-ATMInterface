084c486053c2df14b8c012492a0ab085a828a549
Dhanraj4003 <dhanrajjaiswal4003@gmail.com>
2026-07-02 01:16:29 +0530

    Task 3 completed

---
+++ ATMInterface.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class ATMInterface {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double balance = 10000;
+        int choice;
+        double amount;
+
+        do {
+            System.out.println("\n===== ATM MENU =====");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.println("Balance: ₹" + balance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    amount = sc.nextDouble();
+                    balance += amount;
+                    System.out.println("Deposit Successful!");
+                    break;
+
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    amount = sc.nextDouble();
+
+                    if (amount <= balance) {
+                        balance -= amount;
+                        System.out.println("Withdrawal Successful!");
+                    } else {
+                        System.out.println("Insufficient Balance!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Thank You for Using ATM!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
